095836e6794eb7697a1afd953e33809f8591b7bc
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2025-09-21 18:04:23 +0000

    Handle tmux plugins

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -32,7 +32,17 @@ install_pkg() {
 }
 
 # --- Core installs ---
-install_pkg fzf direnv zoxide bat jq git ripgrep
+install_pkg fzf direnv zoxide bat jq git ripgrep tmux
+
+# --- Tmux plugins ---
+# start a server but don't attach to it
+tmux start-server
+# create a new session but don't attach to it either
+tmux new-session -d
+# install the plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+# killing the server is not required, I guess
+tmux kill-server
 
 # --- uv installer ---
 if ! has_cmd uv; then
