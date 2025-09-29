7d0ab03cef18f7e749b89248197b728a9fa20359
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2025-09-29 23:22:19 +0100

    install eza correctly

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -35,6 +35,14 @@ if has_cmd apt; then
   if has_cmd sudo; then SUDO="sudo"; else SUDO=""; fi
 
   # eza (repo version)
+  $SUDO apt update -y
+  $SUDO apt install -y gpg
+
+  $SUDO mkdir -p /etc/apt/keyrings
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | $SUDO gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | $SUDO tee /etc/apt/sources.list.d/gierens.list
+  $SUDO chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  $SUDO apt update
   $SUDO apt install -y eza
 
   # Neovim (latest via PPA)
